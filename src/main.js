c2a94a908c8b54a6af9e0838aeb1f3efae096676
fcxmarquez <francisco.marquez.solt@gmail.com>
2022-10-09 15:17:29 -0500

    Built page

---
+++ src/main.js
@@ -0,0 +1,51 @@
+//youtube id UCsXVk37bltHxD1rDPwtNM8Q
+const url =
+  "https://youtube-v31.p.rapidapi.com/search?channelId=UCsXVk37bltHxD1rDPwtNM8Q&part=snippet%2Cid&order=date&maxResults=50";
+
+const content = null || document.getElementById("video-list");
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5b5e11a75cmshdfc3aa7839165b2p14ef90jsn04fd3043e856",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(url);
+    let view = `
+    ${videos.items
+      .map(
+        (video) => `
+      <div class="group relative" onclick='() => alert('hi')'>
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
+          >
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full" />
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `
+      )
+      .slice(0, 10)
+      .join("")}
+    `;
+    console.log(videos);
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
